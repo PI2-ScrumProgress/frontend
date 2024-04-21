3e8aa4359dabe570ec9d43b9b3a33caa9f21e930
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-04-21 15:38:44 -0500

    Add login api imtegration

---
+++ api/auth.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import api from "./config/api";
+
+interface LoginResponse {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export async function login(
+  email: string,
+  password: string
+): Promise<LoginResponse> {
+  try {
+    const response = await api.post<LoginResponse>("/auth/login", {
+      email,
+      password,
+    });
+    const tokens = response.data;
+    localStorage.setItem("accessToken", tokens.accessToken);
+    localStorage.setItem("refreshToken", tokens.refreshToken);
+    return tokens;
+  } catch (error: any) {
+    throw new Error(error.response.data);
+  }
+}
--- components/Login.tsx
+++ components/Login.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { login } from "@/api/auth";
 
-const Login = () => {
+const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const data = await login(email, password);
+      console.log("Login success", localStorage.getItem("accessToken"));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center h-screen bg-gray-100">
       <div className="p-6 max-w-sm w-full bg-white shadow-md rounded-md">
         <div className="mb-4">
           <h2 className="text-center text-2xl font-bold">Iniciar Sesión</h2>
         </div>
-        <form>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleLogin();
+          }}
+        >
           <div className="mb-5">
             <label htmlFor="email" className="block mb-2 text-sm text-gray-600">
               Correo Electrónico
@@ -17,6 +35,8 @@ const Login = () => {
               name="email"
               id="email"
               placeholder="E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               className="px-4 py-2 w-full border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
             />
           </div>
@@ -32,6 +52,8 @@ const Login = () => {
               name="password"
               id="password"
               placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
               className="px-4 py-2 w-full border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
             />
           </div>

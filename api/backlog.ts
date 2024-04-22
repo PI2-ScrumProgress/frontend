81a9a1ce0cb697a5f534a81686f3edae63f066e2
Sebastian Saldarriaga <ssaldarriaga.cardona@udea.edu.co>
2024-04-21 19:06:10 -0500

    Add create user story function

--- api/backlog.ts
+++ api/backlog.ts
@@ -1,6 +1,6 @@
 import api from "./config/api";
 
-interface TaskCreateResponse {
+interface BacklogElementResponse {
   id: string;
   projectId: string;
   elementType: string;
@@ -17,15 +17,41 @@ export async function createTask(
   description?: string,
   priority?: string,
   dueDate?: string
-): Promise<TaskCreateResponse> {
+): Promise<BacklogElementResponse> {
   try {
-    const response = await api.post<TaskCreateResponse>("tasks/create", {
+    const response = await api.post<BacklogElementResponse>("tasks/create", {
       projectId,
       title,
       description,
       priority,
       dueDate,
     });
+    return response.data;
+  } catch (error: any) {
+    throw new Error(error.response.data);
+  }
+}
+
+export async function createStory(
+  projectId: string,
+  title: string,
+  description?: string,
+  priority?: string,
+  acceptanceCriteria?: string,
+  storyPoints?: number
+): Promise<BacklogElementResponse> {
+  try {
+    const response = await api.post<BacklogElementResponse>(
+      "user-stories/create",
+      {
+        projectId,
+        title,
+        description,
+        priority,
+        acceptanceCriteria,
+        storyPoints,
+      }
+    );
     return response.data;
   } catch (error: any) {
     throw new Error(error.response.data);
